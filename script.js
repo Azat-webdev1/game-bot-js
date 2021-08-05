bfc410bbb0bc369d3bccb14fe1ee9999a5ff28c2
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-05 12:18:54 +0400

    feat: создал checkGameWish для проверки, хочешь играть или нет

--- script.js
+++ script.js
@@ -1,11 +1,45 @@
 'use strict';
 
+//проверка на число
 const isNum = (num) => {
   return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
-const gameBot = () => {
+//создание игры
+const сreateGame = () => {
   let randomNum = parseInt(Math.random() * 100);
-  let userNum = +prompt('Угадай число от 1 до 100');
+  console.log(randomNum);
+  let userNum = prompt('Угадай число от 1 до 100');
+  
+  //игровой бот
+  const gameBot = () => {
 
+    //проверка, хочешь играть или нет
+    let checkGameWish = (flag) => {
+      if (flag) {
+        getNumber()
+      } else {
+        alert('Спасибо, что поиграли со мной')
+      }
+    };
+
+    if (isNum(userNum > randomNum)) {
+      alert('Загаданное число меньше');
+      //+prompt('Ввести новый вариант');
+    } else if (isNum(userNum < randomNum)) {
+      alert('Загаданное число больше');
+      //+prompt('Ввести новый вариант');
+    } else if (typeof userNum === 'string') {
+      alert('Введи число!');
+      //+prompt('Ввести новый вариант');
+    } else if (userNum === null) {
+      alert('Игра окончена');
+    } else if (isNum(userNum === randomNum)) {
+      alert('Поздравляю, Вы угадали!!!')
+    }
+  };
+  
+  return gameBot();
 };
+
+сreateGame();
