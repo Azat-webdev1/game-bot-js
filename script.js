445d1f5353eaf9aa6838df8401cf786681503432
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-05 11:23:16 +0400

    feat: создал фукции gameBot и isNum

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const isNum = (num) => {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+const gameBot = () => {
+  let randomNum = parseInt(Math.random() * 100);
+  let userNum = +prompt('Угадай число от 1 до 100');
+
+};
