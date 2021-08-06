5d6fad78673cb8925a2aa9931f067630d2d32187
Azat-webdev <62510440+Azat-webdev@users.noreply.github.com>
2021-08-07 01:01:51 +0400

    fix: исправил баги

--- script.js
+++ script.js
@@ -3,65 +3,62 @@
 //создание игры
 const сreateGame = () => {
   let lives = 10;
-  
+  //let count = 0;
+
   let randomNum = parseInt(Math.floor((Math.random() * 101)));
-  
+  console.log('Загадоное число: ', randomNum);
   
   //проверка на число
   const isNum = (num) => {
     return !isNaN(parseFloat(num)) && isFinite(num);
   };
 
-  const gameOver = () => {
-    alert('Игра окончена.');
-  };
-  
-  const letAnotherGame = () => {
-    confirm('Попытки закончились, хотите сыграть еще?')
-      ? сreateGame()
-      : gameOver();
-  };
-
-  const gameWin = () => {
-    confirm(`Поздравляю, Вы угадали!!! Осталось попыток ${lives}
-      Хотели бы сыграть еще?`) ?
-      сreateGame()
-      : gameOver();
-  };
-
-  const getAnswer = (userNum) => {
-    if (lives === 0) {
-      return letAnotherGame();
-    }
-    userNum = +prompt('Введи число от 1 до 100.');
-    console.log(`Попытка: ${userNum}`);
-    if (!isNum(userNum) || userNum > 100) {
-      return getAnswer();
-    }
-
-    //игровой бот
-    const gameBot = (lives) => {
+  //игровой бот
+  const gameBot = () => {
+    lives--;
     
-      if (userNum === randomNum) {
+    let userNum = +prompt('Введи число от 1 до 100.');
+    
+    const gameOver = () => {
+      alert('Игра окончена.');
+    };
+    
+    const letAnotherGame = () => {
+      confirm('Попытки закончились, хотите сыграть еще?')
+        ? сreateGame()
+        : gameOver();
+    };
+  
+    const gameWin = () => {
+      confirm(`Поздравляю, Вы угадали!!! Осталось попыток ${lives}
+        Хотели бы сыграть еще?`) ?
+        сreateGame()
+        : gameOver();
+    };
+  
+      if (lives === 0) {
+        return letAnotherGame();
+      }
+      else if (!isNum(userNum) || userNum > 100) {
+        return gameBot();
+      }
+      else if (userNum === randomNum) {
         gameWin();
       }
-      if (userNum === 0) {
+      else if (userNum === 0) {
         gameOver();
-        return;
       }
-      if (userNum < randomNum) {
+      else if (userNum < randomNum) {
         alert(`Загаданное число больше, осталось попыток ${lives}.`);
-        getAnswer();
+        gameBot();
       }
-      if (userNum > randomNum) {
+      else if (userNum > randomNum) {
         alert(`Загаданное число меньше, осталось попыток ${lives}.`);
-        getAnswer();
-      }
-      return gameBot(--lives);
-    };
+        gameBot();
+    }
+    
   };
-  
-  confirm('Угадай число от 1 до 100.') ? getAnswer() : gameOver();
+  gameBot();
 };
 
 сreateGame();
